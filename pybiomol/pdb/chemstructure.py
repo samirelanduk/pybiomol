882f8c59f01b2252aa5b4c76b71f7fa591fad754
Sam Ireland <sam.ireland.uk@gmail.com>
2016-04-22 11:58:16 +0100

    Atom can get distance to other atom, and repr

--- pybiomol/pdb/chemstructure.py
+++ pybiomol/pdb/chemstructure.py
@@ -1,9 +1,23 @@
+import math
+
 class Atom:
 
     def __init__(self, element, atom_id, x, y, z, name=None):
-        self.element = element
+        self.element = element.title()
         self.atom_id = atom_id
         self.x = float(x)
         self.y = float(y)
         self.z = float(z)
         self.name = name if name else element
+
+
+    def __repr__(self):
+        return "<%s Atom>" % self.element
+
+
+    def distance_to(self, other_object):
+        x_sum = math.pow((other_object.x - self.x), 2)
+        y_sum = math.pow((other_object.y - self.y), 2)
+        z_sum = math.pow((other_object.z - self.z), 2)
+        distance = math.sqrt(x_sum + y_sum + z_sum)
+        return distance
--- tests/pdb/test_chemstructure.py
+++ tests/pdb/test_chemstructure.py
@@ -19,6 +19,23 @@ class AtomTest(unittest.TestCase):
         self.assertIsInstance(atom.y, float)
         self.assertIsInstance(atom.z, float)
         self.assertIsInstance(atom.name, str)
+        self.assertEqual(atom.name, atom.element)
+
+
+    def test_can_make_atom_with_name(self):
+        atom = self.get_atom(name="AT1")
+        self.assertEqual(atom.name, "AT1")
+
+
+    def test_can_get_distance_between_atoms(self):
+        atom1 = self.get_atom(xyz=(-0.791, 64.789, 30.59)) # Atom 2621 in 1LOL
+        atom2 = self.get_atom(xyz=(5.132, 63.307, 56.785)) # Atom 1011 in 1LOL
+        pymol_calculated_distance = 26.9
+        self.assertAlmostEqual(
+         atom1.distance_to(atom2),
+         pymol_calculated_distance,
+         delta=0.01
+        )
 
 
 
