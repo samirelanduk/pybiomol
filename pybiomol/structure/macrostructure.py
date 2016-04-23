b698b1546f9c20b3581a7ebfdd58a345d0804775
Sam Ireland <sam.ireland.uk@gmail.com>
2016-04-23 19:59:31 +0100

    Add ResiduicStructure class

--- pybiomol/structure/macrostructure.py
+++ pybiomol/structure/macrostructure.py
@@ -9,3 +9,18 @@ class Residue(Molecule):
 
     def __repr__(self):
         return "<Residue (%s)>" % self.name
+
+
+
+class ResiduicStructure(AtomicStructure):
+
+    def __init__(self, residues):
+        self.residues = residues
+        atoms = []
+        for residue in self.residues:
+            atoms += residue.atoms
+        AtomicStructure.__init__(self, *atoms)
+
+
+    def __repr__(self):
+        return "<ResiduicStructure (%i residues)>" % len(self.residues)
--- tests/structure/test_macrostructure.py
+++ tests/structure/test_macrostructure.py
@@ -4,16 +4,30 @@ sys.path.append(".")
 import pybiomol
 from test_chemstructure import ChemTest
 
+class MacroTest(ChemTest):
 
-class ResidueTest(ChemTest):
-
-    def test_can_make_resiude(self):
+    def get_residue(self):
         atoms = [self.get_atom() for _ in range(10)]
         for index, atom in enumerate(atoms[:-1]):
             atom.covalent_bond_to(atoms[index+1])
         residue = pybiomol.Residue("VAL", atoms)
+        return residue
+
+
+class ResidueTest(MacroTest):
+
+    def test_can_make_resiude(self):
+        residue = self.get_residue()
         str(residue)
 
+
+
+class ResiduicStructureTest(MacroTest):
+
+    def test_can_make_residuic_structure(self):
+        residues = [self.get_residue() for _ in range(10)]
+        residuic_structure = pybiomol.ResiduicStructure(residues)
+        str(residuic_structure)
 
 
 if __name__ == "__main__":
